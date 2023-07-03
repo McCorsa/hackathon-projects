83c4c9d8be3f77605d4225a365bd6f94d87c0cf6
Corey McCormick <coreymccor@live.com.au>
2023-07-03 15:07:02 +1000

    Added enhance to forms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { enhance } from "$app/forms";
     import type { Team } from "@prisma/client";
     import type { PageData, ActionData } from "./$types";
     export let data: PageData;
@@ -30,19 +31,40 @@
     }
 </script>
 
-<header class="bg-primary p-2 flex flex-col md:flex-row flex-wrap items-center gap-1">
+<header
+    class="bg-primary p-2 flex flex-col md:flex-row flex-wrap items-center gap-1"
+>
     <span class="grow text-white">Winter Hackathon</span>
     {#if data.user}
-    <form method="post" action="?/logout">
-        <span class="text-white text-sm">Welcome {data.user.name}</span>
-        <button class="btn btn-xs" type="submit">Logout</button>
-    </form>
+        <form method="post" action="?/logout" use:enhance>
+            <span class="text-white text-sm">Welcome {data.user.name}</span>
+            <button class="btn btn-xs" type="submit">Logout</button>
+        </form>
     {:else}
-    <form class="flex flex-col md:flex-row gap-1 w-full md:w-auto" method="post" action="?/login">
-        <input type="text" name="name" id="name" bind:value={name} class="input input-sm" placeholder="Name">
-        <input type="password" name="pword" id="pword" bind:value={pword} class="input input-sm" placeholder="Password">
-        <button type="submit" class="btn btn-sm">Submit</button>
-    </form>
+        <form
+            class="flex flex-col md:flex-row gap-1 w-full md:w-auto"
+            method="post"
+            action="?/login"
+            use:enhance
+        >
+            <input
+                type="text"
+                name="name"
+                id="name"
+                bind:value={name}
+                class="input input-sm"
+                placeholder="Name"
+            />
+            <input
+                type="password"
+                name="pword"
+                id="pword"
+                bind:value={pword}
+                class="input input-sm"
+                placeholder="Password"
+            />
+            <button type="submit" class="btn btn-sm">Submit</button>
+        </form>
     {/if}
 </header>
 
@@ -71,34 +93,70 @@
         <div class="flex flex-row p-1 items-center gap-2">
             <h2 class="text-lg font-bold grow">Teams to Join</h2>
             {#if filtering}
-                <button class="btn btn-error btn-sm" on:click={() => {filtering = false; filter = "";}}>Clear Filter</button>
+                <button
+                    class="btn btn-error btn-sm"
+                    on:click={() => {
+                        filtering = false;
+                        filter = "";
+                    }}>Clear Filter</button
+                >
             {:else}
-                <button class="btn btn-primary btn-sm" on:click={() => filtering = true}>Filter</button>
+                <button
+                    class="btn btn-primary btn-sm"
+                    on:click={() => (filtering = true)}>Filter</button
+                >
             {/if}
             {#if creatingTeam}
-                <button class="btn btn-error btn-sm" on:click={() => creatingTeam = false}>Cancel</button>
+                <button
+                    class="btn btn-error btn-sm"
+                    on:click={() => (creatingTeam = false)}>Cancel</button
+                >
             {:else}
-                <button class="btn btn-success btn-sm" on:click={() => {creatingTeam = true;}}>New Team</button>
+                <button
+                    class="btn btn-success btn-sm"
+                    on:click={() => {
+                        creatingTeam = true;
+                    }}>New Team</button
+                >
             {/if}
         </div>
         {#if creatingTeam}
-            <form class="flex flex-row px-2 py-1 gap-1 bg-base-300 rounded-xl" transition:slide on:submit={createTeam}>
-                <input class="input grow" placeholder="New Team Name" bind:value={newTeam} autofocus>
+            <form
+                class="flex flex-row px-2 py-1 gap-1 bg-base-300 rounded-xl"
+                transition:slide
+                on:submit={createTeam}
+            >
+                <input
+                    class="input grow"
+                    placeholder="New Team Name"
+                    bind:value={newTeam}
+                    autofocus
+                />
                 <button class="btn btn-success" type="submit">Create</button>
             </form>
         {/if}
         {#if filtering}
             <div class="px-2 py-1 bg-base-300 rounded-xl" transition:slide>
-                <input type="text" class="input input-bordered w-full" placeholder="Filter" bind:value={filter} autofocus>
+                <input
+                    type="text"
+                    class="input input-bordered w-full"
+                    placeholder="Filter"
+                    bind:value={filter}
+                    autofocus
+                />
             </div>
         {/if}
-        {#each teams.filter(val => val.name.toLowerCase().startsWith(filter.toLowerCase())) as team}
-            <div class="collapse collapse-plus bg-base-300 hover:bg-base-200 border border-base-100 transition">
-                <input type="radio" name="my-accordion-4" /> 
+        {#each teams.filter((val) => val.name
+                .toLowerCase()
+                .startsWith(filter.toLowerCase())) as team}
+            <div
+                class="collapse collapse-plus bg-base-300 hover:bg-base-200 border border-base-100 transition"
+            >
+                <input type="radio" name="my-accordion-4" />
                 <div class="collapse-title text-lg font-bold">
                     {team.name} ({team.id})
                 </div>
-                <div class="collapse-content"> 
+                <div class="collapse-content">
                     <p>hello</p>
                 </div>
             </div>
